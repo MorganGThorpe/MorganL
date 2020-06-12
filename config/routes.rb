2886e7027926ab496c0b2edb803136c27c1d8abe
Morgan Lizop <morgan.lizop@iese.net>
2020-06-12 13:14:53 +0100

    added links to each page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'education', to: 'pages#education'
+  get 'photography', to: 'pages#photography'
+  get 'web_project', to: 'pages#web_project'
+  get 'adventures', to: 'pages#adventures'
+  get 'work', to: 'pages#work'
+  get 'secret_page', to: 'pages#secret_page'
 end
