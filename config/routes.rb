fe0fef268fe2209d7dc41fc4946a0c0c7269265f
Morgan Lizop <morgan.lizop@iese.net>
2020-08-14 16:00:41 +0100

    changed the style of the navbar and made it responsive

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'education', to: 'pages#education'
   get 'photography', to: 'pages#photography'
-  get 'web_project', to: 'pages#web_project'
+  get 'web_projects', to: 'pages#web_projects'
   get 'adventures', to: 'pages#adventures'
   get 'work', to: 'pages#work'
   get 'secret_page', to: 'pages#secret_page'
